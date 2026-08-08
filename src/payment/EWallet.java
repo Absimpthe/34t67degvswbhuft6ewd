5991ffe4592be920f4b3945c9314d00089cfe994
KzYuen <94364477+KzYuen@users.noreply.github.com>
2026-08-08 22:49:29 +0800

    on the way, inform passenger to add a "wallet"

--- src/payment/EWallet.java
+++ src/payment/EWallet.java
@@ -2,11 +2,11 @@ package payment
 
 public EWallet implements Payment{
   @Override
-  public boolean pay(double payment){
-    
-    
-    
+  public boolean pay(double amount){
+    if(amount <= 0){
+      System.out.print(" E-Wallet has invalid amount");
     }
+    if(
 
 
 
