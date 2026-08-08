5a3f980110824f66df4c2589f581b85a60e802cf
KzYuen <94364477+KzYuen@users.noreply.github.com>
2026-08-08 22:20:51 +0800

    Enhance CardPayment with cardNumber field and methods
    Added cardNumber field and constructor to CardPayment class. Implemented getCardNumber method.

--- src/payment/CardPayment.java
+++ src/payment/CardPayment.java
@@ -1,8 +1,21 @@
 package payment;
 
 public class CardPayment implements Payment{
-  @Override
+    private double cardNumber;
+
+    public double getCardNumber{
+      return cardNumber;
+    }
+    public CardPayment(double cardNumber){
+      this.cardNumber = cardNumber;
+    }  
+    
+    @Override
     public boolean pay(double amount) {
+        if(cardNumber == null // cardNumber.trim().length() < 14){
+           System.out.print("The card is not verified / Invalid card number". Payment Failed");
+            return false;
+        }  
         System.out.printf("Processing cash payment of RM %.2f...%n", amount);
         System.out.println("Payment amount: RM " + amount);
         System.out.println("Cash payment successful.");
