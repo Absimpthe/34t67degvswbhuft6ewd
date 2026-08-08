90b2f4763082d917f96d0243b33f0abf77a524e7
KzYuen <94364477+KzYuen@users.noreply.github.com>
2026-08-08 22:00:23 +0800

    Override pay method for cash payment processing

--- src/payment/CashPayment.java
+++ src/payment/CashPayment.java
@@ -1,5 +1,11 @@
 package payment;
 
 public class CashPayment {
-
+  @Override
+    public boolean pay(double amount) {
+        System.out.printf("Processing cash payment of RM %.2f...%n", amount);
+        System.out.println("Payment amount: RM " + amount);
+        System.out.println("Cash payment successful.");
+        return true;
+    }
 }
