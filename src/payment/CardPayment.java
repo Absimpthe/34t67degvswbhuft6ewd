0ef71b1d0f415b2005e1cca46f5c20db0be90893
Absimpthe <cedro755@gmail.com>
2026-08-09 22:54:24 +0800

    fixed syntax errors

--- src/payment/CardPayment.java
+++ src/payment/CardPayment.java
@@ -1,19 +1,19 @@
 package payment;
 
 public class CardPayment implements Payment{
-    private double cardNumber;
+    private String cardNumber;
 
-    public double getCardNumber{
+    public String getCardNumber(){
       return cardNumber;
     }
-    public CardPayment(double cardNumber){
+    public CardPayment(String cardNumber){
       this.cardNumber = cardNumber;
     }  
     
     @Override
     public boolean pay(double amount) {
-        if(cardNumber == null // cardNumber.trim().length() < 14){
-           System.out.print("The card is not verified / Invalid card number". Payment Failed");
+        if(cardNumber == null || cardNumber.trim().length() < 14){
+           System.out.print("The card is not verified / Invalid card number. Payment Failed");
             return false;
         }  
         System.out.printf("Processing cash payment of RM %.2f...%n", amount);
