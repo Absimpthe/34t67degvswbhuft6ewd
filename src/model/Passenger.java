9f59765423940338f5200467b0963515c0becad0
remy-ma <9844remy@gmail.com>
2026-08-14 12:41:27 +0800

    Update Passenger.java

--- src/model/Passenger.java
+++ src/model/Passenger.java
@@ -6,9 +6,6 @@ public class Passenger extends User {
 
     private double balance;
 
-    public Passenger() {
-    }
-
     public Passenger(String userId,String name,String email,String password,double balance) {
         //父类User
         super(userId, name, email, password, UserRole.PASSENGER);
