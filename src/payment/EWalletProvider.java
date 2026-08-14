fad34e9eb79d753d385923c2d1f9945839f9608f
CynCET <cyncet06@gmail.com>
2026-08-14 11:30:06 +0800

    Update EWalletProvider.java

--- src/payment/EWalletProvider.java
+++ src/payment/EWalletProvider.java
@@ -1,5 +1,5 @@
 package payment;
-//using enum to "lock" it
+//using enum to "lock" it, if write wrong then compile-error
 public enum EWalletProvider { 
   
     TOUCH_N_GO("Touch 'n Go eWallet"),
@@ -16,7 +16,7 @@ public enum EWalletProvider {
     public String getLabel(){
         return label;
     }
-    // return display label
+    // overriding toString() to concatenation the string later on to print a full label instead of just "touch 'n go"
     @Override
     public String toString(){
         return label;
